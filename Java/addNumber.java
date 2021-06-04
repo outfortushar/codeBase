a690cc84774928eac51e80956a2a392484b61387
outfortushar <85346600+outfortushar@users.noreply.github.com>
2021-06-04 17:15:08 +0530

    Modified Print messsage

--- Java/addNumber.java
+++ Java/addNumber.java
@@ -4,7 +4,7 @@ class MyClass {
   public static void main(String[] args) {
     int x, y, sum;
     Scanner myObj = new Scanner(System.in); // Create a Scanner object
-    System.out.println("Type a number:");
+    System.out.println("Type a number below:");
     x = myObj.nextInt(); // Read user input
 
     System.out.println("Type another number below:");
